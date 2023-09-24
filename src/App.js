6bf02d26e331dfb7a0f222f34fd63ecf7954cb98
camilo carvajal <camilocc597@gmail.com>
2023-09-23 23:31:32 -0500

    creating task component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import Add from './components/Add';
+import TaskList from './components/TaskList';
 import './App.css';
 import './styles/Add.css'
+import './styles/Task.css'
 
 function App() {
   return (
     <div className="App">
+      <TaskList/>
+      <TaskList/>
       <Add/>
     </div>
   );
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,10 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 
-function TaskList()  {
+function TaskList() {
 
-    
+    const [task, setTask] = useState([]);
+
+    return (
+        <div className="task-box">
+            <input
+                type="text"
+                className="input-task">
+
+            </input>
+            <div>
+                <button
+                    className="complete-task"
+                    type="button">
+                    Completed
+                </button>
+                <button
+                    className="delete-task"
+                    type="button">
+                    Eliminar
+                </button>
+            </div>
+        </div>
+    );
 
 }
- 
+
 export default TaskList;
